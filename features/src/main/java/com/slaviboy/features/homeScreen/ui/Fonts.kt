7b88178891ff2f5f3b690e63e0c59dd00ddeea76
slaviboy <slavi94slavi94@gmail.com>
2022-08-07 03:48:44 +0300

    Add custom font

---
+++ features/src/main/java/com/slaviboy/features/homeScreen/ui/Fonts.kt
@@ -0,0 +1,22 @@
+package com.slaviboy.features.homeScreen.ui
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.slaviboy.features.R
+
+val NotoSansFont = FontFamily(
+    Font(R.font.noto_sans_black, FontWeight.Black),
+    Font(R.font.noto_sans_black_italic, FontWeight.Black, FontStyle.Italic),
+    Font(R.font.noto_sans_bold, FontWeight.Bold),
+    Font(R.font.noto_sans_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.noto_sans_light, FontWeight.Light),
+    Font(R.font.noto_sans_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.noto_sans_medium, FontWeight.Medium),
+    Font(R.font.noto_sans_medium_italic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.noto_sans_regular, FontWeight.Normal),
+    Font(R.font.noto_sans_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.noto_sans_thin, FontWeight.Thin),
+    Font(R.font.noto_sans_thin_italic, FontWeight.Thin, FontStyle.Italic)
+)
